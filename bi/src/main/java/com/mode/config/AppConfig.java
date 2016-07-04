75d9a97a4cfa7398be67a837328d416e49af63b8
hgrourou <hgrourou>
2016-07-04 18:45:51 +0800

    1.fix API.2.modify the controller js.

---
+++ bi/src/main/java/com/mode/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.mode.config;
+
+/**
+ * Created by kang on 2016/7/4.
+ */
+public class AppConfig {
+
+    // Types list stats users
+    public final static Integer LIST_STATS_DAILY = 1;
+    public final static Integer LIST_STATS_WEEKLY = 2;
+    public final static Integer LIST_STATS_MONTHLY = 3;
+    public final static Integer LIST_ACTIVE_USER_WEEKLY = 4;
+    public final static Integer LIST_ACTIVE_USER_MONTHLY = 5;
+
+}
